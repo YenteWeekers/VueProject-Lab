6748cd81f6d8aa91b07c425809f022673a09c281
YenteWeekers <y.weekers@sintlucasedu.nl>
2025-05-22 13:33:31 +0200

    les 3
    kleine img's en dingjes langs main foto. 3 kleine foto's en begin voor onderste tekst.
    (note: pas alle space around aan naar in between en maar de margin iets groter aan links en recht voor een preciezen strakken lijn aan de linkse kant)

--- LabVueProject/vue-projectLab/src/App.vue
+++ LabVueProject/vue-projectLab/src/App.vue
@@ -44,8 +44,60 @@ import TheWelcome from './components/TheWelcome.vue'
 
 
 
-  <div>
-    <img src="../Img/pexels-andrew-8960464.jpg" height="532" width="auto"/></div>
+  <div class="front">
+    <img class="front-img" src="../Img/pexels-andrew-8960464.jpg" height="532" width="auto"/>
+    <div class="right-sideImg">
+      <div class="social-icons">
+        <img src="../Img/letter-m.png" height="32" width="32"/>
+        <img src="../Img/facebook%20(3).png" height="32" width="32"/>
+        <img src="../Img/twitter.png" height="32" width="32"/>
+      </div>
+      <div class="scroll-down">
+        <img src="../Img/mouse.png" height="37" width="37"/>
+        <p>Scroll Down</p>
+        <div class="scrollDown-line"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="imgs">
+    <img src="../Img/desk.png" height="382" width="344"/>
+    <img src="../Img/chair.png" height="384" width="342"/>
+    <div class="img-groenvak-imgs">
+      <img src="../Img/lamp.png" height="370" width="344"/>
+      <div class="groen-vak-imgs"></div>
+    </div>
+  </div>
+
+
+  <div class="under-text">
+    <div class="leftText-underText">
+      <p class="h1">Free to Choice your Furniture</p>
+      <p>Create your workspace, a place where all your ideas and work will be developed.<br>
+      Choose according to your taste and with quality materials will make your dream<br>
+      workspace come true</p>
+    </div>
+    <div class="rightNumbers-underText">
+      <div class="desk">
+        <h1>25</h1>
+        <p>Desk<br>
+           Collection</p>
+      </div>
+      <div class="line-rightNumbers"></div>
+      <div class="chair">
+        <h1>50</h1>
+        <p>Chair<br>
+          Collection</p>
+      </div>
+      <div class="line-rightNumbers"></div>
+      <div lamp="desk">
+        <h1>15</h1>
+        <p>Lamp<br>
+          Collection</p>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
@@ -96,6 +148,7 @@ display: flex;
 .nav-bar p{
   font-size: 19px;
   font-family: "Open Sans", sans-serif;
+  color: #5a5858;
 }
 .nav-bar{
   display: flex;
@@ -106,9 +159,9 @@ display: flex;
 .nav-items {
   font-size: 18px;
   font-family: "Open Sans", sans-serif;
-  font-weight: bold;
   display: flex;
   flex-direction: row;
+  font-weight: bold;
   gap: 18px;
 }
 .navLine-splitItems{
@@ -116,5 +169,105 @@ display: flex;
   width: 1px;
   background-color: rgb(134 134 134 / 21%);
 
+}
+.front{
+  display: flex;
+  align-items: anchor-center;
+  justify-content: space-around;
+
+}
+.front-img{
+  height: 695px;
+  width: 1200px;
+  color: #36585c;
+}
+.right-sideImg{
+  display: flex;
+  flex-direction: column;
+  gap: 120px;
+}
+
+.social-icons{
+  display: flex;
+  flex-direction: column;
+  gap: 19px;
+}
+.social-icons img{
+  width: 25px;
+  height: auto;
+}
+.scroll-down{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+}
+
+.scroll-down p{
+  font-size: 19px;
+  font-family: "Open Sans", sans-serif;
+  color: #363535;
+  writing-mode: sideways-rl;
+  margin: 0;
+}
+.scrollDown-line{
+  background-color: #898888;
+  width: 1px;
+  height: 280px;
+}
+
+.imgs{
+  display: flex;
+  justify-content: space-around;
+  margin-top: 100px;
+}
+
+.groen-vak-imgs{
+  height: 370px;
+  width: 150px;
+  background-color: #36585c;
+}
+
+.img-groenvak-imgs{
+  display: flex;
+}
+
+.under-text{
+  display: flex;
+}
+
+
+.leftText-underText p{
+  font-size: 17px;
+  font-family: "Open Sans", sans-serif;
+  color: #363535;
+}
+
+.h1{
+  font-weight: bold;
+  font-size: 32px !important;
+}
+
+.rightNumbers-underText{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 28%;
+}
+h1{
+  font-size: 60px;
+  font-family: "Open Sans", sans-serif;
+  color: #363535;
+  font-weight: normal;
+  margin: 0;
+}
+.rightNumbers-underText p{
+  font-family: "Open Sans", sans-serif;
+  margin: 0;
+}
+.line-rightNumbers{
+  height: 107px;
+  width: 1px;
+  background-color: #cccccc;
 }
 </style>
